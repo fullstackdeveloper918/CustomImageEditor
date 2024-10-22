d3fe4771f6e4a0f970906e26bf47cdfa0be57d03
Abhay <abhay@cybersify.tech>
2024-10-22 17:55:51 +0530

    code push

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import ImageEditor from './ImageEditor';
-import Cart from './Cart';
+import ImageEditor from './Components/ImageEditor';
+import Cart from './Components/Cart';
 
 function App() {
   return (
